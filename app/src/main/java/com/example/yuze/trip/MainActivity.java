5bb661d701149b71f3b12a585f9e22be47ab7a98
yzyzt <1280116476@qq.com>
2018-04-21 15:48:11 +0800

    MapView

---
+++ app/src/main/java/com/example/yuze/trip/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.yuze.trip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.TextureMapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextureMapView mMapView;
+    private BaiduMap mBaiduMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        mMapView = (TextureMapView) findViewById(R.id.mTexturemap);
+        mBaiduMap = mMapView.getMap();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mMapView.onPause();
+    }
+}
